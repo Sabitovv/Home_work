b2f7ddc6a6bf8e44d213553fb6cb72f73101cc86
Sabitovv <166857532+Sabitovv@users.noreply.github.com>
2025-11-29 11:38:06 +0500

    Practice task 13 week

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
